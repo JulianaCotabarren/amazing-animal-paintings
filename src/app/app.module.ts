1e3d7eb69a7d46cd1fd9d670a5c75e9acf0f4d45
Juliana Cotabarren <julianacotabarren@gmail.com>
2024-04-08 14:35:31 -0300

    feat: show products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductModule } from './product/product.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, ProductModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
