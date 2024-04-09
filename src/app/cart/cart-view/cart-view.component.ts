fa24d18349aba090fbf8ad13175556df83b1a70c
Juliana Cotabarren <julianacotabarren@gmail.com>
2024-04-09 14:29:01 -0300

    feat: add cart functionalities

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -9,12 +9,32 @@ import { Product } from 'src/app/models/product';
 })
 export class CartViewComponent implements OnInit {
   cartItems: Product[] = [];
+  totalPrice: number = 0;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartService.getCartItems().subscribe((data) => {
       this.cartItems = data;
+      this.totalPrice = this.getTotalPrice();
     });
+  }
+
+  getTotalPrice(): number {
+    let totalPrice = 0;
+    for (let cartItem of this.cartItems) {
+      totalPrice += cartItem.price;
+    }
+    return totalPrice;
+  }
+
+  clearCart(): void {
+    this.cartService.clearCart().subscribe(() => {
+      alert('Product removed');
+    });
+  }
+
+  checkout(): void {
+    this.cartService.checkout(this.cartItems).subscribe();
   }
 }
